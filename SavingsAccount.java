0cc12c6017c3eeccaabd39ffe595e5e8bcda20e5
Md. Saiful Islam <86137224+Saiful-cu@users.noreply.github.com>
2024-07-02 21:03:24 +0600

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -6,16 +6,19 @@ public class SavingsAccount  extends BankAccount{
 	
 	
 	
-	@Override
-	public void withdraw(double amount) {
+		@Override
+	        public void withdraw(double amount) {
 		
 		if(amount <= withdrawalLimit) {
 			initialBalance -= amount;
 			System.out.println(amount+ " tk Withdraw Successfully");
 		}
-		else if(amount> withdrawalLimit) {
+		else if(amount <= 0) {
 			
-			System.out.println("Please enter amount in withdrawal limit");
+			System.out.println("Please enter a valild amount ");
+		}
+		else {
+			System.out.println("Please enter amount in withdrawal limit ");
 		}
 		
 		
